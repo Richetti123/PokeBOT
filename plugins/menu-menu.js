8ce5ac8ca121eec87b063af2b64521e2f62a3672
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2023-12-18 22:21:42 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -106,7 +106,7 @@ let menu = `${lenguajeGB['smsConfi2']()} *${user.genero === 0 ? '👤' : user.ge
 
 ⊜ *Experiencia ➟* ${exp} ⚡
 ⊜ *Diamantes ➟* ${limit} 💎
-⊜ *MichiCoins ➟* ${money} 🐈
+⊜ *PokeCoins ➟* ${money} 🐈
 ⊜ *Tokens ➟* ${joincount} 🪙
 ${readMore}
 *╭━〔 INFORMACIÓN DE MICHIBOT 〕⬣*
@@ -119,34 +119,6 @@ ${readMore}
 ┃💫➺ _${usedPrefix}´owner_
 ┃💫➺ _${usedPrefix}velocidad_
 ┃💫➺ _Bot_ 
-*╰━━━━━━━━━━━━⬣*
-
-*╭━〔 MAPAS DE FREE FIRE Y LISTAS PARA VS 〕⬣*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
-┣ ඬ⃟ 💟 .vs8 Lista de 8 vs 8 
-┣ ඬ⃟ 💟 .vs12 Lista de 12 vs 12
-┣ ඬ⃟ 💟 .vs16 Lista de 16 vs 16
-┣ ඬ⃟ 💟 .vs20 Lista de 20 vs 20
-┣ ඬ⃟ 💟 .vs24 Lista de 24 vs 24
-┣ ඬ⃟ 💟 .fem18 Lista de vivido para las 18🇦🇷
-┣ ඬ⃟ 💟 .fem19 Lista de vivido para las 19🇦🇷
-┣ ඬ⃟ 💟 .fem20 Lista de vivido para las 20🇦🇷
-┣ ඬ⃟ 💟 .fem21 Lista de vivido para las 21🇦🇷
-┣ ඬ⃟ 💟 .fem22 Lista de vivido para las 22🇦🇷
-┣ ඬ⃟ 💟 .fem23 Lista de vivido para las 23🇦🇷
-┣ ඬ⃟ 💟 .fem00 Lista de vivido para las 00🇦🇷
-┣ ඬ⃟ 💟 .masc18 Lista de vivido para las 18🇦🇷
-┣ ඬ⃟ 💟 .masc19 Lista de vivido para las 19🇦🇷
-┣ ඬ⃟ 💟 .masc20 Lista de vivido para las 20🇦🇷
-┣ ඬ⃟ 💟 .masc21 Lista de vivido para las 21🇦🇷
-┣ ඬ⃟ 💟 .masc22 Lista de vivido para las 22🇦🇷
-┣ ඬ⃟ 💟 .masc23 Lista de vivido para las 23🇦🇷
-┣ ඬ⃟ 💟 .masc00 Lista de vivido para las 00🇦🇷
-┣ ඬ⃟ 💟 .bermuda Mapa de Bermuda de Free Fire
-┣ ඬ⃟ 💟 .purgatorio Mapa de Purgatorio de Free Fire
-┣ ඬ⃟ 💟 .kalahari Mapa de Kalahari de Free Fire
-┣ ඬ⃟ 💟 .alpes Mapa de Alpes de Free Fire
-┣ ඬ⃟ 💟 .nexterra Mapa de Nexterra de Free Fire
 *╰━━━━━━━━━━━━⬣*
 
 *╭━〔 REPORTAR COMANDO 〕━⬣*
